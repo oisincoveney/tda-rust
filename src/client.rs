fddb173d24d480920ac38ae677b38dfa1139474c
Oisin Coveney <oisinthec@gmail.com>
2020-05-01 12:40:48 -0700

    sorta understand wtf I'm doing now

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,4 @@
 extern crate reqwest;
-extern crate state;
 
 /**
  *
@@ -7,14 +6,10 @@ extern crate state;
  *
  * */
 pub mod client {
-
-    use state::Storage;
-    use std::sync::RwLock;
-
     type RClient = reqwest::Client;
 
     lazy_static! {
-        pub static ref CLIENT: Storage<RwLock<reqwest::Client>> = Storage::new();
+        static ref GO: RClient = RClient::new();
     }
 
     pub struct Client {
@@ -24,42 +19,9 @@ pub mod client {
     }
 
     impl Client {
-        fn create(
-            access_code: String,
-            refresh_code: String,
-            key: String,
-        ) -> &'static RwLock<reqwest::Client> {
-            if CLIENT.try_get().is_none() {
-                let c = Client {
-                    access_code,
-                    refresh_code,
-                    key,
-                };
-
-                CLIENT.set(RwLock::new(reqwest::Client::new()));
-            }
-
-            CLIENT.get()
-        }
-
-        fn go() {
-            if CLIENT.try_get().is_none() {
-                panic!("No client found.");
-            }
-            CLIENT.get();
-        }
-
-        fn get(&self, link: &String) {
-            Client::go();
-            // .get(link.to_string())
-            // .bearer_auth(self.access_code)
-            // .query(&[("apikey", self.key)]);
-        }
-
-        // fn post(&self, &link) {}
-
-        // fn put(&self, &link) {}
-
-        // fn delete(&self, &link) {}
+        fn get() {}
+        fn post() {}
+        fn put() {}
+        fn delete() {}
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,10 @@ extern crate lazy_static;
 
 mod client;
 
-use client::client::CLIENT;
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
-        penis.get("https://www.google.com");
     }
 }
