394bbee36660e5c7a86e22db00f53ad4c98f8c44
Oisin Coveney <oisincoveney@gmail.com>
2020-05-05 21:52:46 -0700

    add options

--- src/option_chains/mod.rs
+++ src/option_chains/mod.rs
@@ -5,6 +5,9 @@ use crate::client::ApiKeyType;
 use std::collections::HashMap;
 
 pub(crate) async fn get_option_chain(options: &HashMap<&str, &str>) -> String {
+    /*
+    GET /v1/marketdata/chains?apikey=VCSOJTWA76YNGGQAMSVVCVADRXWNWXU8&symbol=SPY&contractType=ALL&strikeCount=30&includeQuotes=TRUE&strategy=SINGLE&range=ALL&fromDate=2020-03-01&toDate=2020-06-05&expMonth=ALL&optionType=ALL HTTP/1.1
+     */
     client::text(
         client::retrieve()
             .get(links::GET_OPTION_CHAIN)
